05e0e39d491f60a63a5ae8c121f22476ce691ffb
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-05-19 11:00:26 -0500

    saves new lists to db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,20 @@
 const notesData = require("../db/db.json");
 const router = require("express").Router();
+const fs = require("fs");
 
 router.get("/notes", (req, res) => {
     res.json(notesData);
 }).post("/notes", function(req, res) {
-    notesData.push(req.body);
-})
+    const newNote = req.body;
+    console.log(newNote);
+    notesData.push(newNote);
+    console.log(notesData);
+    fs.writeFile("./db/db.json", JSON.stringify(notesData), err => {
+        if (err) throw err;
+
+        console.log("File saved");
+    });
+    res.json(newNote);
+});
 
 module.exports = router;
