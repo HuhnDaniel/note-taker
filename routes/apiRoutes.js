b0b905132acef9ea50d52c2f54c5dfa23bdfc246
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-05-19 11:18:26 -0500

    added unique ids to each note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,14 +6,17 @@ router.get("/notes", (req, res) => {
     res.json(notesData);
 }).post("/notes", function(req, res) {
     const newNote = req.body;
+    newNote.id = notesData.length;
     console.log(newNote);
+
     notesData.push(newNote);
-    console.log(notesData);
+
     fs.writeFile("./db/db.json", JSON.stringify(notesData), err => {
         if (err) throw err;
 
         console.log("File saved");
     });
+
     res.json(newNote);
 });
 
