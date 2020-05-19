4697c704c5a348b4111bf56a8ae2496303b46d0e
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-05-19 09:21:27 -0500

    refactoring html and api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const notesData = require("../db/db.json");
 const router = require("express").Router();
 
-router.get("/notes", function(req, res) {
+router.get("/notes", (req, res) => {
     res.json(notesData);
 }).post("/notes", function(req, res) {
     notesData.push(req.body);
