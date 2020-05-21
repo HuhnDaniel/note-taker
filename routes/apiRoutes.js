e27ea86abe265177675c56fea8da12aed843056f
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-05-21 18:29:18 -0500

    add note deleting functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,13 +2,17 @@ const notesData = require("../db/db.json");
 const router = require("express").Router();
 const fs = require("fs");
 
+// Direct to retrieve api notes
 router.get("/notes", (req, res) => {
     res.json(notesData);
-}).post("/notes", function(req, res) {
+})
+// Direct to post api notes
+.post("/notes", (req, res) => {
     const newNote = req.body;
     newNote.id = notesData.length;
     console.log(newNote);
 
+    // Move the new note into the notes array then save it to db.json
     notesData.push(newNote);
 
     fs.writeFile("./db/db.json", JSON.stringify(notesData), err => {
@@ -18,6 +22,21 @@ router.get("/notes", (req, res) => {
     });
 
     res.json(newNote);
+});
+
+// Direct to delete a specific note
+router.delete("/notes/:id", (req, res) => {
+    const chosenId = req.params.id;
+
+    notesData.splice(chosenId, 1);
+
+    fs.writeFile("./db/db.json", JSON.stringify(notesData), err => {
+        if (err) throw err;
+
+        console.log("File saved");
+    });
+
+    res.end();
 });
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const path = require("path");
 
+// Direct to notes.html
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
+// Direct all other requests to index.html
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
